197b129abd6754c71fa0a923559a93738827fe1a
Olivier FANOMEZANTSOA <olivier.fanomezantsoa@proximitybbdo.fr>
2020-03-19 21:48:18 +0400

    Groupe details page amelioration.

--- src/components/contents/Albums/AlbumDetails.js
+++ src/components/contents/Albums/AlbumDetails.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
@@ -9,6 +8,9 @@ import TabsInfoAlbum from "./TabsInfoAlbum";
 import groupe from "../../../data/mettalica";
 import ".././Contents.css"
 import Urls from "../Urls";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import {Link} from "react-router-dom";
 
 const useStyles = makeStyles({
     root: {
@@ -27,25 +29,57 @@ export default function AlbumDetails(props) {
     const album = albums.length ? albums[0] : {};
     const cover = album.cover ? album.cover.xl : groupe.picture.xl;
     const info = album.publicationDate ? album.publicationDate : album.length ? album.dateRelease : album.deezerFans + " Fans";
+    const sep = <span className="span">:</span>;
 
     return (
         <>
             <Card elevation={0} className={classes.root}>
-                <CardActionArea disableRipple>
-                    <CardMedia className={classes.media}
-                               image={cover}
-                               component="img"
-                               title={album.title}
-                    />
-                    <CardContent>
+                <CardMedia className={classes.media}
+                           image={cover}
+                           component="img"
+                           title={album.title}
+                />
+                <CardContent>
+                    <Paper className={"album-details-paper"}>
                         <Typography gutterBottom variant="h5" component="h2">
-                            {album.title}
+                            {album.title} ({album.songs.length} titles)
                         </Typography>
                         <Typography variant="span" component="h4">
                             {info}
                         </Typography>
-                    </CardContent>
-                </CardActionArea>
+                    </Paper>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Duration {sep}</th>
+                            <td>{album.length}</td>
+                            <th>Group {sep}</th>
+                            <td><Link to={"/"}>{album.name}</Link></td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th>Genre {sep}</th>
+                            <td>{album.genre}</td>
+                            <th>Country {sep}</th>
+                            <td>{album.country}</td>
+                        </tr>
+                        <tr>
+                            <th>Date realese {sep}</th>
+                            <td>{album.dateRelease}</td>
+                            <th>Publication date {sep}</th>
+                            <td>{album.publicationDate}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <List className={classes.list}>
+                        {album.availableCountries?.map((countrie, i) => (
+                            <li key={`section-${i}`} className={classes.listSection}>
+                                <a href="#">{countrie}</a>
+                            </li>
+                        ))}
+                    </List>
+                </CardContent>
             </Card>
             <Card elevation={0}>
                 <Urls urls={album}/>
